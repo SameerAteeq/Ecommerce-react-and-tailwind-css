58f775e9b6b5c81ead44bd9b3afe99a4a134bc8f
SameerAteeq <sameerateeq0@gmail.com>
2023-03-14 14:15:37 +0500

    change

--- src/components/wrapper/Wrapper.js
+++ src/components/wrapper/Wrapper.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Wrapper = ({ children }) => {
-  return <div className="w-full max-w-7xl my-0 mx-auto">{children}</div>;
+  return <div className="w-full max-w-[1340px] my-0 mx-auto">{children}</div>;
 };
 
 export default Wrapper;
